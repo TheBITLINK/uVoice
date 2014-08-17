c6662ddcfc32a02a32c9e14d41187ac1956b1b86
TheBITLINK aka BIT <me@thebitlink.com>
2014-08-16 22:55:29 -0430

    Pasandome de VS a GitHub
    El Team Explorer de Visual Studio es una m****a

---
+++ UTAUdotNET/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace UTAUdotNET
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Un truquito para que la ventana se vea bonita C:
+        WindowRendering wdr = new WindowRendering();
+        DispatcherTimer DoubleClickCount = new DispatcherTimer();
+        bool Clicked { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Clicked = false;
+            DoubleClickCount.Interval = TimeSpan.FromMilliseconds(200);
+            DoubleClickCount.Tick += DoubleClickCount_Tick;
+        }
+
+        void DoubleClickCount_Tick(object sender, EventArgs e)
+        {
+            Clicked = false;
+            DoubleClickCount.Stop();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            wdr.ExtendDwm(this);
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!Clicked)
+            {
+                DragMove();
+                Clicked = true;
+                DoubleClickCount.Start();
+                return;
+            }
+            else
+            {
+                if (WindowState == System.Windows.WindowState.Maximized)
+                {
+                    this.WindowState = System.Windows.WindowState.Normal;
+                    Clicked = false;
+                    return;
+                }
+                if (WindowState == System.Windows.WindowState.Normal)
+                {
+                    this.WindowState = System.Windows.WindowState.Maximized;
+                    Clicked = false;
+                    return;
+                }     
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 VentanadeLocura = new Window1();
+            VentanadeLocura.Show();
+        }
+    }
+}
---
+++ UTAUdotNET/WindowGraphics.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace UTAUdotNET
+{
+    public class WindowRendering
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MARGINS
+        {
+            public int cxLeftWidth;      // width of left border that retains its size 
+            public int cxRightWidth;     // width of right border that retains its size 
+            public int cyTopHeight;      // height of top border that retains its size 
+            public int cyBottomHeight;   // height of bottom border that retains its size
+        };
+
+
+        [DllImport("DwmApi.dll")]
+        public static extern int DwmExtendFrameIntoClientArea(
+            IntPtr hwnd,
+            ref MARGINS pMarInset);
+
+        public void ExtendDwm(Window wnd)
+        {
+            try
+            {
+                // Obtain the window handle for WPF application
+                IntPtr mainWindowPtr = new WindowInteropHelper(wnd).Handle;
+                HwndSource mainWindowSrc = HwndSource.FromHwnd(mainWindowPtr);
+                mainWindowSrc.CompositionTarget.BackgroundColor = Color.FromArgb(0, 0, 0, 0);
+
+                // Get System Dpi
+                System.Drawing.Graphics desktop = System.Drawing.Graphics.FromHwnd(mainWindowPtr);
+                float DesktopDpiX = desktop.DpiX;
+                float DesktopDpiY = desktop.DpiY;
+
+                // Set Margins
+                WindowRendering.MARGINS margins = new WindowRendering.MARGINS();
+
+                // Extend glass frame into client area 
+                // Note that the default desktop Dpi is 96dpi. The  margins are 
+                // adjusted for the system Dpi.
+                margins.cxLeftWidth = 5;
+                margins.cxRightWidth = 5;
+                margins.cyTopHeight = Convert.ToInt32((72 + 5) * (DesktopDpiX / 96));
+                margins.cyBottomHeight = 5;
+
+                int hr = WindowRendering.DwmExtendFrameIntoClientArea(mainWindowSrc.Handle, ref margins);
+                // 
+                if (hr < 0)
+                {
+                    //DwmExtendFrameIntoClientArea Failed
+                }
+            }
+            // If not Vista, paint background white. 
+            catch (DllNotFoundException)
+            {
+                Application.Current.MainWindow.Background = Brushes.Black;
+            }
+        }
+    }
+}
